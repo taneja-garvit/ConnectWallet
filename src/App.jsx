eb6d70b0e0a1cbd27a80338bee87acad5ab2f3c4
Garvit <taneja.garvit29@gmail.com>
2024-06-21 15:29:50 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,60 @@
+import { createWeb3Modal, defaultConfig } from '@web3modal/ethers5/react'
+import ConnectButton from './ConnectWallet'
+
+// 1. Get projectId
+const projectId = '29fa5b8dbe55e7aaa7a0ef6baa46156b'
+
+// 2. Set chains
+const mainnet = {
+  chainId: 1,
+  name: 'Ethereum',
+  currency: 'ETH',
+  explorerUrl: 'https://etherscan.io',
+  rpcUrl: 'https://cloudflare-eth.com'
+}
+const sepolia = {
+  chainId: 11155111,
+  name: 'Sepolia',
+  currency: 'ETH',
+  explorerUrl: 'https://sepolia.etherscan.io',
+  rpcUrl: 'https://rpc.sepolia.org',
+};
+
+// 3. Create a metadata object
+const metadata = {
+  name: 'My Website',
+  description: 'My Website description',
+  url: 'https://mywebsite.com', // origin must match your domain & subdomain
+  icons: ['https://avatars.mywebsite.com/']
+}
+
+// 4. Create Ethers config
+const ethersConfig = defaultConfig({
+  /*Required*/
+  metadata,
+
+  /*Optional*/
+  enableEIP6963: true, // true by default
+  enableInjected: true, // true by default
+  enableCoinbase: true, // true by default
+  rpcUrl: '...', // used for the Coinbase SDK
+  defaultChainId: 1 // used for the Coinbase SDK
+})
+
+// 5. Create a Web3Modal instance
+createWeb3Modal({
+  ethersConfig,
+  chains: [mainnet,sepolia],
+  projectId,
+  enableAnalytics: true // Optional - defaults to your Cloud configuration
+})
+
+function App() {
+  return (
+    <>
+      <ConnectButton/>
+    </>
+  );
+}
+
+export default App;
